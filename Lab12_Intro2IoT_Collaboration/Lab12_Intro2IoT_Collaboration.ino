2e6a78d1c2abd298a6e3dd642bce391e53ed428e
DanielFisk1234 <daniel.dilalla@dawsoncollege.qc.ca>
2026-04-23 16:44:53 -0400

    Update Lab12_Intro2IoT_Collaboration.ino

--- Lab12_Intro2IoT_Collaboration/Lab12_Intro2IoT_Collaboration.ino
+++ Lab12_Intro2IoT_Collaboration/Lab12_Intro2IoT_Collaboration.ino
@@ -39,7 +39,7 @@ bool isRoomDark() {
   Serial.println(lightLevel);
 
   // TODO: Fix this logic so it returns true when it's dark
-  return false; // <-- incorrect for now
+  return lightLevel < darkThreshold; // <-- incorrect for now
 }
 
 // ===============================
